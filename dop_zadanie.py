37e65f34470b41e7ad0f5def7fe759b6cdd60efb
Dmitriy <zhilexpert@gmail.com>
2024-11-28 00:39:05 +0400

    Add files via upload

--- dop_zadanie.py
+++ dop_zadanie.py
@@ -1,7 +1,8 @@
 
 import random
-
-first_field = random.randint(3, 20)
+first_field = (3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+first_field = random.choice(first_field)
+# first_field = random.randint(3, 20) думал ещё так сделать, возможно так правильней?
 result = []
 for i in range(1,first_field):
     for j in range(2,first_field):
